5d239d8ab00c844866989d673b301100411d8ffb
tomasff <15079464+tomasff@users.noreply.github.com>
2023-12-02 17:33:02 +0000

    Day 2: Part two, clean up a bit

--- src/day_two.rs
+++ src/day_two.rs
@@ -1,33 +1,50 @@
 use std::cmp::max;
 
-const RED: u32 = 12;
-const GREEN: u32 = 13;
-const BLUE: u32 = 14;
+const MAX_ALLOWED_RED: u32 = 12;
+const MAX_ALLOWED_GREEN: u32 = 13;
+const MAX_ALLOWED_BLUE: u32 = 14;
 
-fn solve_part_one(input: &str) -> Option<u32> {
-    input.lines()
+fn parse_game<'input>(input: &'input str) -> impl Iterator<Item = (u32, (u32, u32, u32))> + 'input {
+    input
+        .lines()
         .map(|game| game.split_once(":").expect("Invalid game!"))
-        .map(|(name, runs)| (
-                name.split_once(" ").and_then(|(_, id)| id.parse::<u32>().ok()),
-                runs.split(";").flat_map(
-                    |run| run.split(",").map(|cubes| cubes.trim().split_once(" ").expect("Invalid game"))
-                )
-                .map(|(count, color)| (count.parse::<u32>().expect("Invalid count"), color))
-                .fold((0, 0, 0), |(max_r, max_b, max_g), (count, color)|
-                    match color {
-                        "red" => (max(max_r, count), max_b, max_g),
-                        "green" => (max_r, max_b, max(max_g, count)),
-                        "blue" => (max_r, max(max_b, count), max_g),
-                        _ => unreachable!()
-                    }
-                )
+        .map(|(name, runs)| {
+            (
+                name.split_once(" ")
+                    .and_then(|(_, id)| id.parse::<u32>().ok())
+                    .expect("Invalid game ID!"),
+                runs.split(";")
+                    .flat_map(|run| {
+                        run.split(",")
+                            .map(|cubes| cubes.trim().split_once(" ").expect("Invalid game"))
+                    })
+                    .map(|(count, color)| (count.parse::<u32>().expect("Invalid count"), color))
+                    .fold(
+                        (0, 0, 0),
+                        |(max_r, max_g, max_b), (count, color)| match color {
+                            "red" => (max(max_r, count), max_g, max_b),
+                            "green" => (max_r, max(max_g, count), max_b),
+                            "blue" => (max_r, max_g, max(max_b, count)),
+                            _ => unreachable!(),
+                        },
+                    ),
             )
-        )
-        .filter(|(_, c)| c.0 <= RED && c.1 <= BLUE && c.2 <= GREEN)
+        })
+}
+
+fn solve_part_one(input: &str) -> u32 {
+    parse_game(input)
+        .filter(|(_, (r, g, b))| {
+            *r <= MAX_ALLOWED_RED && *g <= MAX_ALLOWED_GREEN && *b <= MAX_ALLOWED_BLUE
+        })
         .map(|(id, _)| id)
         .sum()
 }
 
+fn solve_part_two(input: &str) -> u32 {
+    parse_game(input).map(|(_, (r, g, b))| r * g * b).sum()
+}
+
 pub fn solve(input: &str) -> (Option<u32>, Option<u32>) {
-    (solve_part_one(input), None)
+    (Some(solve_part_one(input)), Some(solve_part_two(input)))
 }
