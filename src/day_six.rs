a0e3ddaa2bba7d6dcb977d034bbfb74e03afacbc
tomasff <15079464+tomasff@users.noreply.github.com>
2023-12-06 17:05:07 +0000

    Day 6: Cleanup + Part 2

--- src/day_six.rs
+++ src/day_six.rs
@@ -1,24 +1,50 @@
-use std::iter::zip;
+fn count_race_solutions(time: f64, distance: f64) -> u32 {
+    let sq = (time * time - 4.0 * distance).sqrt() / 2.0;
+
+    ((time / 2.0 + sq).ceil() - (time / 2.0 - sq).floor()) as u32 - 1
+}
 
 fn solve_part_one(input: &str) -> u32 {
-    let races: Vec<Vec<f32>> = input
-        .lines()
-        .filter_map(|line| {
-            line.split_once(":").map(|(_, values)| {
-                values
-                    .split_whitespace()
-                    .filter_map(|number| number.parse().ok())
-                    .collect()
-            })
+    input
+        .split_once('\n')
+        .map(|(times, distances)| {
+            times
+                .split_whitespace()
+                .skip(1)
+                .zip(distances.split_whitespace().skip(1))
+                .filter_map(|(time, distance)| match (time.parse(), distance.parse()) {
+                    (Ok(t), Ok(d)) => Some((t, d)),
+                    _ => None,
+                })
+                .map(|(time, distance)| count_race_solutions(time, distance))
+                .product()
         })
-        .collect();
+        .expect("Invalid puzzle input.")
+}
 
-    zip(&races[0], &races[1])
-        .into_iter()
-        .map(|(time, distance)| (time.powi(2) - 4.0 * distance).sqrt().ceil() as u32)
-        .product()
+fn solve_part_two(input: &str) -> u32 {
+    input
+        .split_once('\n')
+        .map(|(times, distances)| {
+            let time: f64 = times
+                .split_whitespace()
+                .skip(1)
+                .collect::<String>()
+                .parse()
+                .expect("Invalid race time.");
+
+            let distance: f64 = distances
+                .split_whitespace()
+                .skip(1)
+                .collect::<String>()
+                .parse()
+                .expect("Invalid race distance.");
+
+            count_race_solutions(time, distance)
+        })
+        .expect("Invalid puzzle input.")
 }
 
 pub fn solve(input: &str) -> (Option<u32>, Option<u32>) {
-    (Some(solve_part_one(input)), None)
+    (Some(solve_part_one(input)), Some(solve_part_two(input)))
 }
